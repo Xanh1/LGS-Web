aa272df8c736646d2fc644197a07267af2ad83e4
Miguel Apolo <apolo.miguel12x@gmail.com>
2025-09-26 11:29:52 -0500

    fix: adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
@@ -11,7 +11,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : '/<tu-repo>',
+			base: dev ? '' : '/LGS-Web',
 		}
 	}
 };
